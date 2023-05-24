137afc5fe217ceecbae5ee8e437f46820d098513
Dustin Dowell <7419947+whaaaley@users.noreply.github.com>
2023-05-23 23:01:40 -0500

    Code style update

--- output-turbo.js
+++ output-turbo.js
@@ -1,118 +1,65 @@
-import { Application, Router } from 'https://deno.land/x/oak/mod.ts';
-import { config } from 'https://deno.land/x/dotenv/mod.ts';
+// index.js
 
-const app = new Application();
-const router = new Router();
-const PORT = 3000;
+import 'https://deno.land/std@0.184.0/dotenv/load.ts'
+import { Application, Router } from 'https://deno.land/x/oak@v11.1.0/mod.ts'
+
+import { translateMessage } from './translation.js';
 
 // Load environment variables from .env file
 config({ export: true });
 
-// Utility function to translate a message using the OpenAI API
-async function translateMessage(message) {
-  const openaiApiUrl = 'https://api.openai.com/v1/engines/davinci-codex/completions';
+const app = new Application();
+const router = new Router();
+const PORT = Deno.env.get('PORT') || 3000;
 
-  const response = await fetch(openaiApiUrl, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${Deno.env.get('OPENAI_API_KEY')}`,
-    },
-    body: JSON.stringify({
-      prompt: `Translate the following message to Spanish: "${message}"`,
-      max_tokens: 50,
-      temperature: 0.7,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0,
-      stop: '\n',
-      n: 1,
-      model: 'gpt-3.5-turbo',
-    }),
-  });
-
-  const data = await response.json();
-  const translatedMessage = data.choices[0].text.trim();
-
-  return translatedMessage;
-}
-
-// Route to handle Discord interaction events
-router.post('/interaction', async (ctx) => {
-  try {
-    const interaction = await ctx.request.body().value;
-
-    // Handle Discord interaction events
-    if (interaction.type === 1) {
-      // Handle PING event
-      ctx.response.status = 200;
-      ctx.response.body = {
-        type: 1,
-      };
-    } else if (interaction.type === 2) {
-      // Handle COMMAND event
-      const message = interaction.data.options[0].value;
-      const translatedMessage = await translateMessage(message);
-
-      // Create the response payload
-      const responsePayload = {
-        type: 4,
-        data: {
-          content: translatedMessage,
-        },
-      };
-
-      // Send the interaction response
-      await sendInteractionResponse(interaction.id, interaction.token, responsePayload);
-
-      ctx.response.status = 200;
-    } else {
-      ctx.response.status = 400;
-      ctx.response.body = 'Bad Request';
-    }
-  } catch (err) {
-    ctx.response.status = err.status || 500;
-    ctx.response.body = err.message;
-  }
+app.addEventListener('listen', ({ hostname, port, secure }) => {
+  console.log(`Listening on http${secure ? 's' : ''}://${hostname}:${port}`);
 });
 
-// Utility function to send an interaction response as a webhook request
-async function sendInteractionResponse(interactionId, interactionToken, responsePayload) {
-  const discordApiUrl = `https://discord.com/api/v10/interactions/${interactionId}/${interactionToken}/callback`;
+app.use(async ({ request, response }, next) => {
+  const origin = request.headers.get('Origin');
+  const allowed = whitelist[origin];
 
-  await fetch(discordApiUrl, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bot ${discordConfig.botToken}`,
-    },
-    body: JSON.stringify(responsePayload),
-  });
-}
-
-// Middleware to handle errors
-app.use(async (ctx, next) => {
-  try {
-    await next();
-  } catch (err) {
-    ctx.response.status = 500;
-    ctx.response.body = 'Internal Server Error';
+  if (allowed) {
+    response.headers.set('Access-Control-Allow-Headers', allowed.headers);
+    response.headers.set('Access-Control-Allow-Methods', allowed.methods);
+    response.headers.set('Access-Control-Allow-Origin', origin);
   }
+
+  await next();
+  console.log(request.method, request.url);
 });
 
-// Error handling middleware
-app.use(async (ctx, next) => {
+router.get('/', ({ response }) => {
+  response.headers.set('Content-Type', 'text/html');
+  response.status = 200;
+  response.body = 'Hello world!';
+});
+
+router.post('/translate', async ({ request, response }) => {
+  const { message } = await request.body().value;
+
+  if (!message) {
+    response.status = 400;
+    response.body = 'Bad Request';
+    return;
+  }
+
   try {
-    await next();
-  } catch (err) {
-    ctx.response.status = err.status || 500;
-    ctx.response.body = err.message;
+    const translatedMessage = await translateMessage(message);
+
+    response.status = 200;
+    response.body = {
+      translatedMessage,
+    };
+  } catch (error) {
+    console.error(error);
+    response.status = 500;
+    response.body = 'Internal Server Error';
   }
 });
 
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-// Start the server
-console.log(`Server running on port ${PORT}`);
 await app.listen({ port: PORT });
