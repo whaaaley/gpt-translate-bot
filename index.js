62b5b8ddde5db3c2198a5be92613d26761e6af63
Dustin Dowell <7419947+whaaaley@users.noreply.github.com>
2023-05-23 23:41:29 -0500

    WebSocket implementation

--- index.js
+++ index.js
@@ -1,65 +1,96 @@
-// index.js
+import 'dotenv/load.ts'
 
-import 'https://deno.land/std@0.184.0/dotenv/load.ts'
-import { Application, Router } from 'https://deno.land/x/oak@v11.1.0/mod.ts'
+// Load Environment Variables
+const env = {
+  OPENAI_API_KEY: Deno.env.get('OPENAI_API_KEY'),
+  DISCORD_TOKEN: Deno.env.get('DISCORD_TOKEN')
+}
 
-import { translateMessage } from './translation.js';
+// Utility function to translate a message using the OpenAI API
+async function translateMessage(message) {
+  const openaiApiUrl = 'https://api.openai.com/v1/engines/davinci-codex/completions';
 
-// Load environment variables from .env file
-config({ export: true });
+  const response = await fetch(openaiApiUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${env.OPENAI_API_KEY}`,
+    },
+    body: JSON.stringify({
+      prompt: `Translate the following message to Spanish: "${message}"`,
+      max_tokens: 50,
+      temperature: 0.7,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+      stop: '\n',
+      n: 1,
+      model: 'gpt-3.5-turbo',
+    }),
+  });
 
-const app = new Application();
-const router = new Router();
-const PORT = Deno.env.get('PORT') || 3000;
+  const data = await response.json();
+  const translatedMessage = data.choices[0].text.trim();
 
-app.addEventListener('listen', ({ hostname, port, secure }) => {
-  console.log(`Listening on http${secure ? 's' : ''}://${hostname}:${port}`);
-});
+  return translatedMessage;
+}
 
-app.use(async ({ request, response }, next) => {
-  const origin = request.headers.get('Origin');
-  const allowed = whitelist[origin];
-
-  if (allowed) {
-    response.headers.set('Access-Control-Allow-Headers', allowed.headers);
-    response.headers.set('Access-Control-Allow-Methods', allowed.methods);
-    response.headers.set('Access-Control-Allow-Origin', origin);
-  }
-
-  await next();
-  console.log(request.method, request.url);
-});
-
-router.get('/', ({ response }) => {
-  response.headers.set('Content-Type', 'text/html');
-  response.status = 200;
-  response.body = 'Hello world!';
-});
-
-router.post('/translate', async ({ request, response }) => {
-  const { message } = await request.body().value;
-
-  if (!message) {
-    response.status = 400;
-    response.body = 'Bad Request';
-    return;
-  }
+// Start listening for new messages
+async function startListening() {
+  const gatewayUrl = 'https://discord.com/api/v10/gateway'
 
   try {
-    const translatedMessage = await translateMessage(message);
+    // Retrieve the WebSocket gateway URL
+    const response = await fetch(gatewayUrl)
+    const data = await response.json()
+    const gateway = data.url
 
-    response.status = 200;
-    response.body = {
-      translatedMessage,
-    };
+    // Open a WebSocket connection to the gateway
+    const ws = new WebSocket(`${gateway}/?v=10&encoding=json`)
+
+    // Event listener for WebSocket connection
+    ws.addEventListener('open', () => {
+      console.log('WebSocket connection opened')
+    })
+
+    // Event listener for WebSocket messages
+    ws.addEventListener('message', async (event) => {
+      const payload = JSON.parse(event.data)
+
+      if (payload.op === 10) {
+        // Perform necessary handshake
+        const { heartbeat_interval } = payload.d
+
+        setInterval(() => {
+          ws.send(JSON.stringify({ op: 1, d: null }))
+        }, heartbeat_interval)
+      }
+
+      if (payload.op === 11) {
+        // Start listening for events
+        ws.send(JSON.stringify({
+          op: 2,
+          d: {
+            token: env.DISCORD_TOKEN,
+            intents: 1 << 0
+          }
+        }))
+      }
+
+      if (payload.op === 0 && payload.t === 'MESSAGE_CREATE') {
+        const message = payload.d
+
+        if (message.channel_id === channelId) {
+          const translatedMessage = await translateMessage(message.content)
+          // Do something with the translated message, such as sending it to another channel
+          console.log('Translated Message:', translatedMessage)
+        }
+      }
+    })
   } catch (error) {
-    console.error(error);
-    response.status = 500;
-    response.body = 'Internal Server Error';
+    console.error('WebSocket connection error:', error)
   }
-});
+}
 
-app.use(router.routes());
-app.use(router.allowedMethods());
-
-await app.listen({ port: PORT });
+// Start listening for new messages
+startListening()
